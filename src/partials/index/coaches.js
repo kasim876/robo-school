0d5bcba6386906a906ee29570c1421d4c0b56a79
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-10-23 13:46:33 +0300

    feature: adaptive coaches section

--- src/partials/index/coaches.js
+++ src/partials/index/coaches.js
@@ -3,9 +3,9 @@ import Swiper, { Navigation, Scrollbar } from 'swiper';
 Swiper.use([ Navigation, Scrollbar ])
 
 new Swiper(".coaches__slider", {
-  slidesPerView: 3,
-  spaceBetween: 40,
-  allowTouchMove: false,
+  slidesPerView: 1.4,
+  spaceBetween: 20,
+  allowTouchMove: true,
   navigation: {
     prevEl: ".coaches__nav-btn_prev",
     nextEl: ".coaches__nav-btn_next",
@@ -14,5 +14,17 @@ new Swiper(".coaches__slider", {
     el: ".coaches__scrollbar",
     draggable: true,
     dragClass: "coaches__scrollbar-drag",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      allowTouchMove: false,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      allowTouchMove: false,
+    }
   }
 })
